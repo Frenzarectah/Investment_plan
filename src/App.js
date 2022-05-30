35b493baea2d31361ae3832323b419e746f38035
Frenzarectah <sorrentino.francesco87@gmail.com>
2022-05-31 00:10:24 +0200

    begin real project

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import './App.css';
 import logo1 from './assets/UNITED.png';
 import logo2 from './assets/PROPERTIES.png';
-import Progress from './components/Progress/Progress';
-import tick from './components/Clock';
-import Compt from './components/Clock';
-import { ReactDOM } from 'react';
-
 
 function App() {
-  const clickin = () => alert("diobestia");
   return (
-    <div className="App flex flex-row w-full h-screen">
-      <div className="w-1/3 h-full bg-[#35A0EE] text-white pt-12 pl-20">
+    <div className="App flex flex-row border border-yellow w-full h-screen">
+      <div className="sm:w-80 md:w-1/3 lg:w-1/3 h-full border border-black bg-[#35A0EE] text-white pt-12 pl-20">
         <div className="flex flex-row">
           <img alt="united" src={logo1}></img>
             <img alt="propertier" src={logo2}></img>
-        </div>
-        <Progress name="nicola" surname="chimenti"/>  
+        </div> 
       </div>
-      <div id="clocki">
-        <Compt/>
-        <Compt/>
-        </div>
     </div>
   );
 }
