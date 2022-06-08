7559739e0b176600d1d440acd763326e3f4b19fa
Frenzarectah <sorrentino.francesco87@gmail.com>
2022-06-08 17:22:17 +0200

    changed citation compt

--- src/components/Citation_box/Citation.js
+++ src/components/Citation_box/Citation.js
@@ -4,7 +4,7 @@ import '../Citation_box/Citation.css';
 const Citation = () =>{
     return(
         <>
-            <div style={{width:"300px",height:"180px"}} className="flex justify-center items-center bg-white montserrat text-[#A4AEB4] rounded">
+            <div style={{width:"320px",height:"200px"}} className="flex justify-center px-[20px] pt-[10px] items-center bg-white montserrat text-[#A4AEB4] text-md rounded">
             <p>We care about your time, that's why we created a 3-stage onboarding 
                     that will not take more than 5 minutes to complete</p>
             </div>               
