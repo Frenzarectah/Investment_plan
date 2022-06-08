18ebc7dd907c915e745846c858bc52c77c96f988
Frenzarectah <sorrentino.francesco87@gmail.com>
2022-06-08 20:40:50 +0200

    trying context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import logo1 from './assets/UNITED.png';
+import React, { useState} from 'react';
 import logo2 from './assets/PROPERTIES.png';
 import Progress from './components/Progress/Progress';
 import Citation from './components/Citation_box/Citation';
+
+export const globale = React.createContext();
+
 function App() {
+  const [page, setPage] = useState(0);
+  
   return (
+    <globale.Provider value={{page,setPage}}>
+    <div>{page}</div>
     <div className="App flex flex-row md:flex-column w-full h-14 md:h-screen">
       <div className="w-full md:w-1/3 h-full bg-[#35A0EE] sm:text-black md:text-white">
         <div id="logo_sect" className="flex flex-row text-left px-[15%] py-[5%] md:pt-12 md:pl-20">
@@ -16,11 +24,12 @@ function App() {
           <Progress descr="Investment Plants"/>
           <Progress descr="Investment Preferences"/>
         </div>
-        <div id="citation_sect" className="pl-20 pt-14">
+        <div id="citation_sect" className="pl-14 pt-14">
           <Citation/>
       </div>
       </div>
     </div>
+    </globale.Provider>
   );
 }
 
--- src/components/Citation_box/Citation.js
+++ src/components/Citation_box/Citation.js
@@ -1,10 +1,14 @@
-import React from "react";
+import { useContext } from "react";
+import { globale } from "../../App";
 import '../Citation_box/Citation.css';
 
 const Citation = () =>{
+    const [page, setPage] = useContext(globale);
     return(
         <>
             <div style={{width:"320px",height:"200px"}} className="flex justify-center px-[20px] pt-[10px] items-center bg-white montserrat text-[#A4AEB4] text-md rounded">
+            <p>{page}</p>
+            <p onClick={()=>setPage(page+1)}>CLICK HERE!</p>
             <p>We care about your time, that's why we created a 3-stage onboarding 
                     that will not take more than 5 minutes to complete</p>
             </div>               
