22af2974a1b9306eebb6b93af759c7ad88c3d84f
Frenzarectah <sorrentino.francesco87@gmail.com>
2022-06-14 12:23:21 +0200

    changed style string with object on Prog/LastProg

--- src/components/Progress/LastProgress.js
+++ src/components/Progress/LastProgress.js
@@ -7,11 +7,11 @@ import rect_vert from './assets/rect_vertical.png';
 
 const LastProgress = (props) =>{
     const descr = props.descr;
-    var op = "opacity-25";
+    let style = {opacity:"0.25"}
     const [page,setPage] = useContext(globale);
-    if(props.num <= page) op="opacity-100";
+    if(props.num <= page) style={opacity:"1"}
     return(
-        <div classname={op}>
+        <div style={style}>
         <div className="text-left flex flex-row text-black md:text-white lg:text-white text-lg montserrat_alt">
                 <div id="placeholder" className="flex justify-center items-center">
                     <img style={{width:"11px",height:"11px"}} src={rect}/>
