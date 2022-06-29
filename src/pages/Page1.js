fd33a9ea8f91289a8805abef17487605bd41bf18
Frenzarectah <sorrentino.francesco87@gmail.com>
2022-06-29 17:33:33 +0200

    added another mod on Page1 compt

--- src/pages/Page1.js
+++ src/pages/Page1.js
@@ -1,11 +1,17 @@
 import React, {useContext } from 'react';
 import { globale } from '../App';
+import '../App.css';
 
 const Page1 = () =>{
-    const [page,setPage] = useContext(globale);
+    const [page,setPage,pageInfo] = useContext(globale);
     return(
-        <div className='border border-black mt-[50px]'>
-        PAGINA
+        <div className='mt-[50px] flex flex-col'>
+            <div className='montserrat-alt text-black extrabold text-[28px]'>
+                {pageInfo[page].title}
+            </div>
+            <div className='mt-[12px] montserrat text-[#A4AEB4] text-md'>
+                {pageInfo[page].subtitle}
+            </div>
         </div>
     )
 }
